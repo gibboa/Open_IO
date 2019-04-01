e4ce72f2c32d7425d6fc848f491b74276ab3fe99
Adam Gibbons <gibboa@rpi.edu>
2019-03-31 20:49:00 -0400

    We have meaningful serve/client communication We have succesfully got the server and clients to exchange data with each other. The next step will be to use the data to make more meaningful gamestate changes.

--- public/js/proto_frontend_scripts.js
+++ public/js/proto_frontend_scripts.js
@@ -26,11 +26,15 @@ function initGame(){
 	//Establishing Connection to Game Server
 	var socket = io.connect('http://localhost:8081');
 	socket.on('connect', function(){
-		alert("you are connected")
+		alert("you are connected");
+		//send the name that the player typed in before starting
+		socket.emit('initPlayer',{
+        	playerName: name
+        });
 	});
 
 	//setInterval calls function fiven by first arg, every x milliseconds given by second arg
-	//so: call update() 30 times a second
+	//so: call update() 30 times a second (basically determines framerate)
 	setInterval(update, 1000/30);
 
 	//add event listener for player input
@@ -39,20 +43,40 @@ function initGame(){
        	var key_code = event.keyCode;
        	//keyCode Reference for likely keys:
        	//'w'=87, 'a'=65, 's'=83, 'd'=68, SHIFT=16, SPACE=32, 'e'=69, 'q'=81
-       	//UP=38, DOWN=40, LEFT=37, RIGHT=39
+       	//UP=38, DOWN=40, LEFT=37, RIGHT=39, ENTER=13
     	switch (key_code) {
-        case 87: alert("send up msg"); break; //move UP
-        case 83: alert("send down msg"); break; //move DOWN
-        case 68: alert("send right msg"); break; //move RIGHT
-        case 65: alert("seng left msg"); break; //move LEFT
-        //default: alert(code); //uncomment to check other key codes
+	        case 87: alert("send up msg"); break; //move UP
+	        case 83: alert("send down msg"); break; //move DOWN
+	        case 68: alert("send right msg"); break; //move RIGHT
+	        case 65: alert("seng left msg"); break; //move LEFT
+	        //default: alert(code); //uncomment to check other key codes
     	}
+        //attempting to send this data to server with emit
+        socket.emit('playerMovement',{
+        	input: key_code
+        	//some time data may come in handy soon...
+        	//perhaps collect state changes for a given time interval and push them 
+        	//out at the same time, to keep everyone in sync???
+        });
+
     }, false);
+
+	//listens for gameStateUpdate message from server (just alerts for testing)
+	//(This works here, but doesn't work when called in update function)
+    socket.on('gameStateUpdate', function(data){
+		alert('message: ' + data + ' received from server to update gamestate');
+	});
 
 }
 
 //update has two phases:
 //	1. update gamestate data based on any changes offered by the server
 //	2. redraw game using the updated gamestate
-function update(){}
+function update(){
+	//DOESNT WORK HERE...might be nice if it did though... maybe a scope issue?
+	//listing for messages concerning 'gameStateUpdate'
+	//socket.on('gameStateUpdate', function(data){
+		//alert("received something from server to update gamestate");
+	//});
+}
 
--- server.js
+++ server.js
@@ -4,12 +4,13 @@ var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
 //players object contains the state of all connected players
-//(will be set up like a python dictionary of dictionaries)
-//maps socket.id to the dictionary-like collection:
+//maps socket.id to the dictionary-structy-like collection:
 //string direction --> left, right, up, or down
 //int x --> x position in pixels
 //int y --> y position in pixels
 //int playerId --> the socket.id of player
+//string name --> name of player entered at game start
+//access by typing 'players[socket.id].direction'
 var players = {};
  
 app.use(express.static(__dirname + '/public'));
@@ -25,21 +26,37 @@ io.on('connection', function (socket) {
   	players[socket.id] = {
     	direction: 'left', //LATER: add code to make it a random cardinal direction
     	//Math.floor(Math.random() * Math.floor(max)), that will return a random number (0,max]
-    	x: Math.floor(Math.random() * 320) + 160, 
+    	x: Math.floor(Math.random() * 320) + 160, //current board is 640px X 640px so
     	y: Math.floor(Math.random() * 320) + 160, //place player randomly between 160px-480px (not too close to edge)
     	playerId: socket.id,
-    	//we would have name data from client but dont know how to get it to the server yet
+    	name: 'Guest' //changes on connection via 'initPlayer' message
   	};
 
-  	// update all other players of the new player
-  	socket.broadcast.emit('newPlayer', players[socket.id]);
+  	socket.on('initPlayer', function(data){
+  		//change name in player object
+  		players[socket.id].name = data.playerName;
+  		console.log('player name, ' + data.playerName + ' recieved from ' + socket.id +', updating player object');
+  		console.log('Player ' + socket.id + ' name updated to ' + players[socket.id].name);
 
-  	// send the players object to the new player
-  	socket.emit('currentPlayers', players);
+  	});
+
+  	//update all other players of the new player
+  	//socket.broadcast.emit('newPlayer', players[socket.id]);
+
+  	//when the client emits a 'playerMovement' msg we catch the data here
+  	socket.on('playerMovement', function(data){
+  		//upon receit, process data, and send new positions to all clients
+  		//NOTE: this isnt actually movement related yet, i just want to send data
+  		console.log('player', socket.id, 'changed direction to', data.input);
+  		io.sockets.emit('gameStateUpdate', players[socket.id].name + "'s direction changed...");
+  	});
+
+  	//send the players object to the new player
+  	//socket.emit('currentPlayers', players);
   
   	socket.on('disconnect', function () {
     	console.log('user '+ socket.id +' disconnected');
-    	// remove this player from our players object
+    	//remove this player from our players object
     	delete players[socket.id];
     	io.emit('disconnect', socket.id);
   	});
